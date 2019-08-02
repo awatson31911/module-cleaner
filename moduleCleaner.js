381c2c1f65f944521fa11613b457c9de246ee7d3
Anthony <awatson31911@gmail.com>
2019-08-02 14:11:18 -0400

    adds to node cleaner func

--- moduleCleaner.js
+++ moduleCleaner.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
-const path = require("path");
+const path = require('path');
 const root = path.parse(process.cwd()).dir;
+const thisDir = path.resolve(path.parse(process.cwd()).dir, path.parse(process.cwd()).base);
 // const os = require("os");
 
 // module.exports.
@@ -15,25 +16,31 @@ const moduleCleaner = (dir, done) => {
 
     if (!pending) { return done(null, deletedDirPaths); }
 
-    list.forEach( (fileOrFolder) => {
-      fileOrFolderPath = path.resolve(dir, fileOrFolder);
-      
+    for (let i = 0; i < list.length; i++) {
+
+      const fileOrFolder = list[i];
+      const fileOrFolderPath = path.resolve(dir, fileOrFolder);
+
       fs.stat(fileOrFolderPath, (err, stat) => {
         if (stat && stat.isDirectory()) {
-          
-          // If node_modules dir, remove dir
-          if (directory === 'node_modules') {
-            deletedDirPaths.push(fileOrFolderPath)
+
+          if (fileOrFolder === 'node_modules') {  // If node_modules dir, remove dir
+            deletedDirPaths = [...deletedDirPaths, fileOrFolderPath];
+
             // /* fs.rmdir(directory, (err) => { console.err(err) }) */  
+            pending--;
+            done(null, deletedDirPaths)
             
-          }
-          
-          // If dir is not hidden, execute a recursive call
-          if (directory[0] != '.') {
+            if (!pending) { done(null, deletedDirPaths); }
             
-            moduleCleaner(fileOrFolder, (err, res) => {
-              deletedDirPaths = [deletedDirPaths, ...res];
-          
+            
+          } else if (fileOrFolder[0] != '.') { // If dir is not hidden, execute a recursive call
+            
+            moduleCleaner(fileOrFolderPath, (err, res) => {
+              if (res.length) {
+                deletedDirPaths = deletedDirPaths.concat(res);
+                console.log(deletedDirPaths)
+              }
               pending--;
               if (!pending) { done(null, deletedDirPaths); }
             });
@@ -44,10 +51,10 @@ const moduleCleaner = (dir, done) => {
           pending--;
           if (!pending) { done(null, deletedDirPaths); }
         }
-
       });
 
-    });
+    }
+
 
   });
 
@@ -58,44 +65,40 @@ const moduleCleaner = (dir, done) => {
 // function filewalker(dir, done) {
 //   let results = [];
 
-//   fs.readdir(dir, function(err, list) {
-//       if (err) return done(err);
+//   fs.readdir(dir, (err, list) => {
+//     if (err) return done(err);
 
-//       var pending = list.length;
+//     let pending = list.length;
 
-//       if (!pending) return done(null, results);
+//     if (!pending) return done(null, results);
 
-//     list.forEach(function (file) {
-//       //file = path.resolve(dir, file);
-      
-//       fs.stat(file, function(err, stat){
-//         console.log(file)
-//               // If directory, execute a recursive call
-//               if (stat && stat.isDirectory()) {
-//                   // Add directory to array [comment if you need to remove the directories from the array]
-//                   results.push(file);
+//     list.forEach( (file) => {
+//       file = path.resolve(dir, file);
 
-//                   filewalker(file, function(err, res){
-//                       results = results.concat(res);
-//                       //if (!--pending) done(null, results);
-//                   });
-//               } else {
-//                   results.push(file);
-//                 pending--;
-//                   if (!pending) done(null, results);
-//               }
+//       fs.stat(file,  (err, stat) => {
+//         // If directory, execute a recursive call
+//         if (stat && stat.isDirectory()) {
+//           // Add directory to array [comment if you need to remove the directories from the array]
+//           results.push(file);
+
+//           filewalker(file, (err, res) => {
+//             results = results.concat(res);
+//             if (!--pending) done(null, results);
 //           });
+//         } else {
+//           results.push(file);
+//           pending--;
+//           if (!pending) done(null, results);
+//         }
+//       });
 //     });
-    
-//     //done(null, results)
+
 //   });
-// };
+// }
 
 
-console.log(moduleCleaner(root, (err, data) => {
-  if (err) { throw err }
+moduleCleaner(thisDir, (err, data) => {
+  if (err) { throw err; }
 
-  console.log(root, data)
-}))
-
-console.log(path.parse(process.cwd()).dir)
+  console.log('final view--->', data);
+});
