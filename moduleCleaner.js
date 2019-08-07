aadb2a20a9653d47beff48a20f47eeafddab3067
Anthony <awatson31911@gmail.com>
2019-08-07 08:35:56 -0400

    fixes bugs

--- moduleCleaner.js
+++ moduleCleaner.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const root = path.parse(process.cwd()).dir;
-const thisDir = path.resolve(path.parse(process.cwd()).dir, path.parse(process.cwd()).base);
+const thisDir = path.parse(process.cwd()).dir;
 // const os = require("os");
 
 // module.exports.
@@ -9,96 +9,64 @@ const moduleCleaner = (dir, done) => {
   let deletedDirPaths = [];
 
   fs.readdir(dir, (err, list) => {
-    console.log(list)
+    //console.log(list)
     if (err) { return done(err); }
 
     let pending = list.length;
 
     if (!pending) { return done(null, deletedDirPaths); }
-
+    
     for (let i = 0; i < list.length; i++) {
-
+      
       const fileOrFolder = list[i];
       const fileOrFolderPath = path.resolve(dir, fileOrFolder);
-
+      
       fs.stat(fileOrFolderPath, (err, stat) => {
+        //console.log(fileOrFolder, fileOrFolderPath)
         if (stat && stat.isDirectory()) {
-
           if (fileOrFolder === 'node_modules') {  // If node_modules dir, remove dir
             deletedDirPaths = [...deletedDirPaths, fileOrFolderPath];
-
-            // /* fs.rmdir(directory, (err) => { console.err(err) }) */  
+            
+            fs.rmdir(fileOrFolderPath, (err) => { console.error(err) })
+            
             pending--;
-            done(null, deletedDirPaths)
-            
-            if (!pending) { done(null, deletedDirPaths); }
-            
+            if (!pending) {
+              done(null, deletedDirPaths);
+            }
             
           } else if (fileOrFolder[0] != '.') { // If dir is not hidden, execute a recursive call
             
             moduleCleaner(fileOrFolderPath, (err, res) => {
-              if (res.length) {
+              if (err) {
+                throw new Error(err);
+              } else if (res.length) {
                 deletedDirPaths = deletedDirPaths.concat(res);
-                console.log(deletedDirPaths)
               }
               pending--;
               if (!pending) { done(null, deletedDirPaths); }
             });
-
+            
+          } else {
+            pending--;
+            if (!pending) { done(null, deletedDirPaths); }
           }
-
+          
         } else {
           pending--;
           if (!pending) { done(null, deletedDirPaths); }
         }
       });
-
+      
     }
-
-
+    
+    
   });
 
 };
 
-
-
-// function filewalker(dir, done) {
-//   let results = [];
-
-//   fs.readdir(dir, (err, list) => {
-//     if (err) return done(err);
-
-//     let pending = list.length;
-
-//     if (!pending) return done(null, results);
-
-//     list.forEach( (file) => {
-//       file = path.resolve(dir, file);
-
-//       fs.stat(file,  (err, stat) => {
-//         // If directory, execute a recursive call
-//         if (stat && stat.isDirectory()) {
-//           // Add directory to array [comment if you need to remove the directories from the array]
-//           results.push(file);
-
-//           filewalker(file, (err, res) => {
-//             results = results.concat(res);
-//             if (!--pending) done(null, results);
-//           });
-//         } else {
-//           results.push(file);
-//           pending--;
-//           if (!pending) done(null, results);
-//         }
-//       });
-//     });
-
-//   });
-// }
-
-
-moduleCleaner(thisDir, (err, data) => {
-  if (err) { throw err; }
+moduleCleaner('/Users/Adubya/Desktop/CODE/Fullstack/Projects/Personal-Site', (err, data) => {
+  if (err) { throw err;  }
 
   console.log('final view--->', data);
 });
+
